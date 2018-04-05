df81a415c2bdb04012bbae8e80746bffa2f9c372
John Heckendorf <john.heckendorf@ta-logistic.de>
2018-04-05 12:56:32 +0200

    Reworked the eventAggregator

--- Kirides.Libs/Events/EventAggregator.cs
+++ Kirides.Libs/Events/EventAggregator.cs
@@ -2,89 +2,188 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading;
 
 namespace Kirides.Libs.Events
 {
     public class EventAggregator : IEventAggregator
     {
-        private readonly ConcurrentDictionary<Type, ICollection<Subscriber>> Subscribers = new ConcurrentDictionary<Type, ICollection<Subscriber>>();
+        Dictionary<Type, object> events = new Dictionary<Type, object>();
+        public IPubSubEvent<TPayload> GetEvent<TPayload>()
+        {
+            var type = typeof(TPayload);
+            lock (events)
+            {
+                if (!events.TryGetValue(typeof(TPayload), out var value))
+                {
+                    value = new PubSubEvent<TPayload>();
+                    events[type] = value;
+                }
+                return (PubSubEvent<TPayload>)value;
+            }
+        }
+    }
 
-        protected virtual IDisposable Subscribe<TPayload>(Action<TPayload> callback, SynchronizationContext synchronizationContext, ThreadOption threadOption)
+    public class PubSubEvent<TPayload> : IPubSubEvent<TPayload>
+    {
+        public int SubscriberCount => Subscribers.Count;
+        private List<EventSub> Subscribers = new List<EventSub>();
+
+        public IDisposable Subscribe(Action<TPayload> callback)
+            => Subscribe(callback, null, ThreadOption.Default);
+
+        public IDisposable Subscribe(Action<TPayload> callback, SynchronizationContext synchronizationContext)
+            => Subscribe(callback, synchronizationContext, ThreadOption.Inherited);
+
+        public IDisposable Subscribe(Action<TPayload> callback, ThreadOption threadOption)
+            => Subscribe(callback, threadOption == ThreadOption.Inherited ? SynchronizationContext.Current : null, threadOption);
+
+        public IDisposable Subscribe(Action<TPayload> callback, SynchronizationContext synchronizationContext, ThreadOption threadOption)
+            => Subscribe(callback, synchronizationContext, threadOption, false);
+
+        public IDisposable Subscribe(Action<TPayload> callback, SynchronizationContext synchronizationContext = null, ThreadOption threadOption = ThreadOption.Default, bool keepReferenceAlive = false)
         {
             if (callback == null) throw new ArgumentNullException(nameof(callback));
 
-            ICollection<Subscriber> eventSubs = Subscribers.GetOrAddSafe(typeof(TPayload), key => new List<Subscriber>());
-            lock (eventSubs)
+            lock (Subscribers)
             {
-                var sub = new Subscriber(callback) { ThreadOption = threadOption, Context = synchronizationContext };
-                eventSubs.Add(sub);
-                return new SubscriptionToken(sub);
+                var sub = new EventSub(callback, synchronizationContext, this, keepReferenceAlive);
+                Subscribers.Add(sub);
+                return sub;
             }
         }
-        public IDisposable Subscribe<TPayload>(Action<TPayload> callback) => Subscribe(callback, ThreadOption.Default);
-        public IDisposable Subscribe<TPayload>(Action<TPayload> callback, ThreadOption threadOption) => Subscribe(callback, SynchronizationContext.Current, threadOption);
-        public IDisposable Subscribe<TPayload>(Action<TPayload> callback, SynchronizationContext synchronizationContext)
+
+        public void Publish(TPayload payload)
         {
-            if (synchronizationContext == null) throw new ArgumentNullException(nameof(synchronizationContext));
-            return Subscribe(callback, synchronizationContext, ThreadOption.Inherited);
+            List<EventSub> eventSubs;
+            lock (Subscribers)
+            {
+                eventSubs = new List<EventSub>(Subscribers);
+            }
+
+            List<EventSub> toRemove = null;
+            foreach (var sub in eventSubs)
+            {
+                if (!sub.Invoke(payload))
+                {
+                    if (toRemove == null)
+                        toRemove = new List<EventSub>();
+                    toRemove.Add(sub);
+                    continue;
+                }
+            }
+            if (toRemove == null || toRemove.Count == 0)
+                return;
+
+            foreach (var item in toRemove)
+                item.Dispose();
         }
 
-        public void Publish<TPayload>() where TPayload : new() => Publish(new TPayload());
-        public void Publish<TPayload>(TPayload payload)
+        public void Unsubscribe(IDisposable token)
         {
-            if (Subscribers.TryGetValue(typeof(TPayload), out ICollection<Subscriber> eventSubs))
+            var sub = token as EventSub;
+            if (sub == null)
+                throw new InvalidOperationException("Invalid Subscriptiontoken!");
+
+            lock (Subscribers)
             {
-                ICollection<Subscriber> referencesToRemove = new List<Subscriber>();
-                lock (eventSubs)
+                Subscribers.Remove(sub);
+            }
+
+            sub.Dispose();
+        }
+
+
+        private class EventSub : IDisposable
+        {
+            public WeakReference Reference { get; private set; }
+            private Action<object, TPayload> _callback;
+            private object _keepAliveReference;
+            private readonly IPubSubEvent<TPayload> _pubSubEvent;
+
+            public ThreadOption ThreadOption { get; set; }
+            public SynchronizationContext Context { get; private set; }
+            public bool IsAlive => Reference == null || (!_disposed && Reference == null) ? true : !_disposed && Reference.IsAlive;
+            private bool _disposed { get; set; }
+
+            public EventSub(Action<TPayload> callback, SynchronizationContext context, IPubSubEvent<TPayload> pubSubEvent, bool keepAlive = false)
+            {
+                var target = callback.Target;
+                if (target == null)
                 {
-                    Subscriber[] momSubs = new Subscriber[eventSubs.Count];
-                    eventSubs.CopyTo(momSubs, 0);
-                    foreach (var sub in momSubs)
+                    Reference = null;
+                }
+                else
+                {
+                    Reference = new WeakReference(target);
+                    if (keepAlive)
                     {
-                        if (!sub.IsAlive)
-                        {
-                            referencesToRemove.Add(sub);
-                            continue;
-                        }
-                        InvokeEvent(sub, payload, sub.ThreadOption);
+                        _keepAliveReference = target;
                     }
                 }
-                if (referencesToRemove.Count == 0)
-                    return;
 
-                lock (eventSubs)
-                    foreach (var item in referencesToRemove)
-                        eventSubs.Remove(item);
+                // Create the "(instance, value) => instance.callback(value)" Lambda
+                var instanceParam = Expression.Parameter(typeof(object), "instance");
+                var valueParam = Expression.Parameter(typeof(TPayload), "value");
+                var functionCallExpression = Expression.Call(target == null ? null : Expression.Convert(instanceParam, target.GetType()), callback.Method, valueParam);
+                var lambdaExpression = Expression.Lambda<Action<object, TPayload>>(functionCallExpression, instanceParam, valueParam);
+                var compiledLambda = lambdaExpression.Compile();
+
+                _callback = compiledLambda;
+                Context = context;
+                _pubSubEvent = pubSubEvent;
+            }
+
+            public bool Invoke(TPayload payload)
+            {
+                if (!IsAlive || _disposed) return false;
+                if (ThreadOption == ThreadOption.Inherited && Context != null)
+                {
+                    Context.Send(state => _callback(Reference?.Target, (TPayload)state), payload);
+                }
+                else
+                {
+                    _callback(Reference?.Target, payload);
+                }
+                return true;
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+            }
+
+            protected void Dispose(bool disposing)
+            {
+                if (!disposing) return;
+                if (_disposed) return;
+                _disposed = true;
+                _pubSubEvent.Unsubscribe(this);
+                _callback = null;
+                _keepAliveReference = null;
+                Context = null;
+                Reference = null;
             }
         }
-        private void InvokeEvent<TPayload>(Subscriber sub, TPayload payload, ThreadOption threadOption = ThreadOption.Inherited)
-        {
-            if (threadOption == ThreadOption.Inherited && sub.Context != null)
-                sub.Context.Post(x => sub.Invoke((TPayload)x), payload);
-            else
-                sub.Invoke(payload);
-        }
-        public void Unsubscribe(IDisposable token) => token.Dispose();
-        public void Unsubscribe<TPayload>(Action<TPayload> callback)
-        {
-            if (Subscribers.TryGetValue(typeof(TPayload), out ICollection<Subscriber> subs))
-                lock (subs)
-                    foreach (var sub in subs)
-                        if (sub.Reference.Target == callback.Target)
-                        {
-                            sub.Dispose();
-                            subs.Remove(sub);
-                            break;
-                        }
-        }
+    }
+
+    public interface IPubSubEvent<TPayload>
+    {
+        IDisposable Subscribe(Action<TPayload> callback);
+        IDisposable Subscribe(Action<TPayload> callback, SynchronizationContext synchronizationContext);
+        IDisposable Subscribe(Action<TPayload> callback, ThreadOption threadOption);
+        IDisposable Subscribe(Action<TPayload> callback, SynchronizationContext synchronizationContext, ThreadOption threadOption);
+        IDisposable Subscribe(Action<TPayload> callback, SynchronizationContext synchronizationContext = null, ThreadOption threadOption = ThreadOption.Default, bool keepReferenceAlive = false);
+        void Publish(TPayload payload);
+        void Unsubscribe(IDisposable token);
     }
 
     public enum ThreadOption
     {
         /// <summary>
         /// Tries to run the Event on the Thread it was subscribed on
-        /// <para />Or the supplied one (for <see cref="IEventAggregator.Subscribe{TPayload}(Action{TPayload}, SynchronizationContext)"/>)
+        /// <para />Or the supplied one (for <see cref="PubSubEvent{TPayload}.Subscribe(Action{TPayload}, SynchronizationContext, ThreadOption)"/>)
         /// <para />Falls back to "Default" if there is no <see cref="SynchronizationContext"/>
         /// </summary>
         Inherited,
@@ -95,91 +194,6 @@ namespace Kirides.Libs.Events
     }
     public interface IEventAggregator
     {
-        IDisposable Subscribe<TPayload>(Action<TPayload> callback);
-        IDisposable Subscribe<TPayload>(Action<TPayload> callback, ThreadOption threadOption);
-        IDisposable Subscribe<TPayload>(Action<TPayload> callback, SynchronizationContext synchronizationContext);
-        void Publish<TPayload>() where TPayload : new();
-        void Publish<TPayload>(TPayload payload);
-        void Unsubscribe<TPayload>(Action<TPayload> callback);
-        void Unsubscribe(IDisposable token);
-    }
-    public class SubscriptionToken : IDisposable
-    {
-        private Subscriber subscriber;
-        public bool IsDisposed { get; private set; }
-
-        public SubscriptionToken(Subscriber reference)
-        {
-            this.subscriber = reference;
-        }
-
-        /// <summary>
-        /// Unsubscribes from the associated Event and removes references
-        /// </summary>
-        public void Dispose()
-        {
-            if (IsDisposed)
-                return;
-            IsDisposed = true;
-            subscriber.Dispose();
-            subscriber = null;
-        }
-    }
-    public class Subscriber : IDisposable
-    {
-        public WeakReference Reference { get; private set; }
-        private Delegate callback;
-        public ThreadOption ThreadOption { get; set; }
-        public SynchronizationContext Context { get; set; }
-        public bool IsAlive => Reference == null || (!_disposed && Reference == null) ? true : !_disposed && Reference.IsAlive;
-        private bool _disposed { get; set; }
-
-        public Subscriber(Delegate callback)
-        {
-            var target = callback.Target;
-            if (target == null)
-            {
-                // Static method
-                this.Reference = null;
-                this.callback = callback;
-            }
-            else
-            {
-                // Instance method
-                this.Reference = new WeakReference(callback.Target);
-                var paramType = callback.Method.GetParameters()[0].ParameterType;
-                var callbackType = typeof(Action<,>).MakeGenericType(target.GetType(), paramType);
-                // Creates a delegate without target.
-                this.callback = Delegate.CreateDelegate(callbackType, callback.Method);
-            }
-        }
-
-        public bool Invoke<TPayload>(TPayload payload)
-        {
-            if (!IsAlive || _disposed) return false;
-
-            object target = null;
-            if (Reference != null) target = Reference.Target;
-
-            if (target == null) this.callback.DynamicInvoke(payload);
-            else this.callback.DynamicInvoke(target, payload);
-
-            return true;
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-        }
-
-        protected void Dispose(bool disposing)
-        {
-            if (!disposing) return;
-            if (_disposed) return;
-            _disposed = true;
-            callback = null;
-            Reference = null;
-            Context = null;
-        }
+        IPubSubEvent<TPayload> GetEvent<TPayload>();
     }
 }
